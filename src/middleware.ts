615715033a2d7013670ff7fbef6f5496c4ccf8d2
amankumarsingh77 <amankumarsingh7702@gmal.com>
2024-02-21 09:35:50 +0530

    Finally fixed webhooks

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes:['/'],
-    ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)", "/api/upload"]
+    publicRoutes:['/',"/api/webhooks/clerk"],
+    ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)", "/api/upload", "/api/webhooks/clerk"]
     
 });
  
