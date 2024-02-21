ba6ee9f047d8bb3ee669cf3a6f90d314ff79e99b
amankumarsingh77 <amankumarsingh7702@gmal.com>
2024-02-21 12:50:16 +0530

    Fix the the fetch error

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -69,7 +69,8 @@ export async function POST(req: Request) {
       lastName: last_name,
       photo: image_url,
     };
-
+    
+    
     const newUser = await createUser(user);
 
     // Set public metadata
