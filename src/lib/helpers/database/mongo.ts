f1cc5898fb5df2184ee08427e9ee14abafda345b
amankumarsingh77 <amankumarsingh7702@gmal.com>
2024-02-21 09:31:43 +0530

    Tried to fix webhooks

--- src/lib/helpers/database/mongo.ts
+++ src/lib/helpers/database/mongo.ts
@@ -1,6 +1,6 @@
 import mongoose, { Mongoose } from 'mongoose';
 
-const MONGODB_URL = process.env.MONGODB_URL||"mongodb://localhost:27017/FileFlush";
+const MONGODB_URL = process.env.MONGODB_URL||"";
 
 interface MongooseConnection {
   conn: Mongoose | null;
