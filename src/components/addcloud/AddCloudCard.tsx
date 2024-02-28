b723e40b557bb02e94bccf3f0a5b65c333077de8
amankumarsingh77 <amankumarsingh7702@gmal.com>
2024-02-28 22:49:51 +0530

    Updated AddCloud feature

--- src/components/addcloud/AddCloudCard.tsx
+++ src/components/addcloud/AddCloudCard.tsx
@@ -1,7 +1,8 @@
 'use client'
 import React, { useEffect } from "react";
-import {Checkbox, Link, User, Chip, cn} from "@nextui-org/react";
-import {signIn} from "next-auth/react"
+import { Checkbox, Link, User, Chip, cn } from "@nextui-org/react";
+import { signIn } from "next-auth/react"
+import { cloudproviders } from "@/lib/constants/cloudproviders";
 export default function AddCloudCard() {
 
 
@@ -17,19 +18,35 @@ export default function AddCloudCard() {
   }
 
   return (
-    
-      <div className="w-96 rounded-lg flex justify-between gap-2 cursor-pointer p-2 bg-gray-200" onClick={()=> signIn('google',{callbackUrl:"http://localhost:3000/dashboard"})}>
-        <User
-          avatarProps={{size: "lg", src: user.avatar}}
-        
-          name={user.name}
-        />
-        <div className="flex flex-col items-end justify-center">
-          <Chip color="success" size="sm" variant="flat">
-            {user.status}
-          </Chip>
-        </div>
-      </div>
-    
+
+    <div className="flex flex-wrap gap-6 p-3">
+      {cloudproviders.map((provider) => {
+        return (
+          <div key={provider.id} className="w-96 rounded-lg flex justify-between gap-2 cursor-pointer p-2 bg-gray-200" onClick={() => signIn('google', { callbackUrl: "http://localhost:3000/dashboard" })}>
+            <User
+              avatarProps={{ size: "lg", src: provider.image }}
+
+              name={provider.name}
+            />
+            <div className="flex flex-col items-end justify-center">
+              {provider.available ?
+                (
+                  <Chip color="success" size="sm" variant="flat">
+                    available
+                  </Chip>
+                ) : (
+                  <Chip color="danger" size="sm" variant="flat">
+                    inactive
+                  </Chip>
+                )}
+
+            </div>
+
+          </div>
+        )
+      })}
+
+    </div>
+
   );
 }
---
+++ src/lib/constants/cloudproviders.ts
@@ -0,0 +1,52 @@
+export const cloudproviders = [
+    {
+        id:1,
+        name:"Google Drive",
+        image:"/google_drive.png",
+        available:true,
+        staus:"available"
+    },
+    {
+        id:2,
+        name:"DropBox",
+        image:"/dropbox.png",
+        available:true,
+        staus:"available"
+    },
+    {
+        id:3,
+        name:"OneDrive",
+        image:"/onedrive.png",
+        available:true,
+        staus:"available"
+    },
+    {
+        id:4,
+        name:"AWS S3",
+        image:"/S3.svg",
+        available:true,
+        staus:"available"
+    },
+    {
+        id:5,
+        name:"Cloudflare R2",
+        image:"/cloudflare.svg",
+        available:true,
+        staus:"available"
+    },
+    {
+        id:6,
+        name:"Wasabi",
+        image:"/wasabi.png",
+        available:true,
+        staus:"available"
+    },
+    {
+        id:7,
+        name:"PCloud",
+        image:"/Pcloud.png",
+        available: false,
+        staus:"available"
+    },
+    
+]
