ef50b848c408aea5d43294420a358efdaab27b07
amankumar7702 <77506995+amankumar7702@users.noreply.github.com>
2024-03-03 00:32:00 +0530

    Fixed env

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
     publicRoutes:['/',"/api/webhooks/clerk","/api/auth/dropbox","/api/auth/dropbox/callback","api/auth/dropbox"],
-    ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)", "/api/upload", "/api/webhooks/clerk","/api/auth/callback/google", "/api/auth/signin"],
-    debug: true 
+    ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)", "/api/upload", "/api/webhooks/clerk","/api/auth/callback/google", "/api/auth/signin"]
 });
  
 export const config = {
